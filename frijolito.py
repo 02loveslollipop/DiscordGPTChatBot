cc5571b4a6205639273bd1cb0a492e3eb69b568c
02loveslollipop <mmonsalve2005@gmail.com>
2023-03-12 22:01:59 -0500

    Eliminacion del token

--- frijolito.py
+++ frijolito.py
@@ -1,3 +1,3 @@
 import os
 class Bean:
-    token = 'MTA4NDY0MDM1NDU3MTEzNzAyNA.GB3_fP.Af_TYVPnqTW66-vZ0awnuyNNox3MvmoxLjNXbU'
+    token = 'MTA4NDY0MDM1NDU3MTEzNzAyNA.GOKJnT.DilDMU7kGfYivq4o9VJ3Cs1ckFICZB2BckAp2k'
