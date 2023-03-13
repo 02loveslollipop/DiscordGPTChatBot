05dd950660f1e782ea7f9148efb325c4b863e22c
02loveslollipop <mmonsalve2005@gmail.com>
2023-03-12 21:54:45 -0500

    Cambio del token

--- frijolito.py
+++ frijolito.py
@@ -1,3 +1,3 @@
 import os
 class Bean:
-    token = 'MTA4NDY0MDM1NDU3MTEzNzAyNA.GQ34ho.fR8L0uz_iM5Au2XNSP3eZZRu7RYcmNQf7bVHLY!'
+    token = 'MTA4NDY0MDM1NDU3MTEzNzAyNA.GB3_fP.Af_TYVPnqTW66-vZ0awnuyNNox3MvmoxLjNXbU'
