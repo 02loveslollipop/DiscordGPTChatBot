58a726a5983cf9cc1cd7298ef8c5f8f270de9fa7
02loveslollipop <mmonsalve2005@gmail.com>
2023-03-16 22:10:07 -0500

    Fix API keys absolute path

--- frijolito.py
+++ frijolito.py
@@ -1,11 +1,11 @@
 import os
 class Bean:
   if os.name == 'nt':
-    with  open('C:\\Users\\Katana GF66 11UC\\Documents\\token.key','r') as file:
+    with  open('..\\token.key','r') as file:
       token = file.read()
       
 class BeanGPT:
   if os.name == 'nt':
-    with  open('C:\\Users\\Katana GF66 11UC\\Documents\\openai.key','r') as file:
+    with  open('..\\openai.key','r') as file:
       token = file.read()
     
